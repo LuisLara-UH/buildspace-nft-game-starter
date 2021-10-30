f9c9b396c7aa7a727f0c4e43efe8a49780b60177
Luis Lara <luis.lara.uh42@gmail.com>
2021-10-29 20:14:38 -0400

    Add Game logic

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xBa59a274123FA81C7877A473165d452FDcEaf081';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
